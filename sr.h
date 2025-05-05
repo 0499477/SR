cf7f208816aafcb76fe61640358c52137923aaa4
0499477 <142571318+0499477@users.noreply.github.com>
2025-05-06 00:05:36 +1000

    Add files via upload

--- sr.h
+++ sr.h
@@ -1,8 +1,3 @@
-#ifndef SR_H
-#define SR_H
-
-#include "emulator.h"
-
 extern void A_init(void);
 extern void B_init(void);
 extern void A_input(struct pkt);
@@ -10,8 +5,7 @@ extern void B_input(struct pkt);
 extern void A_output(struct msg);
 extern void A_timerinterrupt(void);
 
-#define BIDIRECTIONAL 0
+/* included for extension to bidirectional communication */
+#define BIDIRECTIONAL 0       /*  0 = A->B  1 =  A<->B */
 extern void B_output(struct msg);
 extern void B_timerinterrupt(void);
-
-#endif
